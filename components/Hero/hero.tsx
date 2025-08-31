8ba37d20d72013ec8fa4b4f2397486488f430c77
Andriy Romanov <mgm.agro04@gmail.com>
2025-08-31 13:11:46 +0300

    edit component hero end global style

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -3,19 +3,41 @@ import styles from "./hero.module.css";
 
 export default function HeroPage() {
   return (
-    <>
-      <section className={styles.hero}>
-        <div className={styles.imagesBlock}>
+    <section className={styles.heroSection}>
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <div className={styles.hero__container}>
+            {/* Заголовок */}
+            <h1 className={`${styles.hero__title} h1`}>
+              Your smart productivity partner
+            </h1>
+
+            {/* Текст */}
+            <p className={`${styles.hero__text} text24`}>
+              Track your tasks, manage your time, and boost productivity with
+              powerful tools designed for students and professionals.
+            </p>
+
+            {/* Кнопка */}
+            <button
+              className={`${styles.hero__btn} btnfullcolor`}
+              type="button"
+            >
+              Start Now
+            </button>
+          </div>
+
+          {/* Зображення */}
           <Image
-            src="/hero/hero.png"
-            alt="Foto page"
-            width={1440}
-            height={739}
-            style={{ width: "100%", height: "auto" }}
+            className={styles.hero__image}
+            src="/hero/desk_girl_at_kanban@2x.webp"
+            alt="image"
+            width={537}
+            height={400}
             priority
           />
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }

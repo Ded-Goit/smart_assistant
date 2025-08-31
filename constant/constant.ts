82a0b20f822d3a60bec67a0d6fa152a8a3a962e8
Andriy Romanov <mgm.agro04@gmail.com>
2025-08-31 16:04:50 +0300

    added policy page

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,4 +1,7 @@
 export const navLinks = [
+  { href: "/cookies-policy", label: "About" },
+  { href: "/terms-and-conditions", label: "About" },
+  { href: "/privacy-policy", label: "About" },
   { href: "/about", label: "About" },
   { href: "/book", label: "About book" },
   { href: "/shiassistant", label: "Shi assistant" },

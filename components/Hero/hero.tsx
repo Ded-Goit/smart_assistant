ae3981a51e60acd1b786faf8bfb327fcfabee77e
Iryna Ielkina <ielkinairinka@gmail.com>
2025-08-31 22:39:32 +0300

    update hero section

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -5,13 +5,25 @@ export default function HeroPage() {
   return (
     <>
       <section className={styles.hero}>
-        <div className={styles.imagesBlock}>
+        <div className={styles.hero__container}>
+          <h1 className={`${styles.hero__title}`}>
+            Your smart productivity partner
+          </h1>
+          <p className={`${styles.hero__text} text 24`}>
+            Track your tasks, manage your time, and boost productivity with
+            powerful tools designed for students and professionals.
+          </p>
+          <button className={`${styles.hero__btn} btnfullcolor`} type="button">
+            Start Now
+          </button>
+        </div>
+        <div className={styles.hero__image}>
           <Image
-            src="/hero/hero.png"
-            alt="Foto page"
-            width={1440}
-            height={739}
-            style={{ width: "100%", height: "auto" }}
+            src="/hero/desk_girl_at_kanban@2x.webp"
+            alt="Girl at kanban"
+            width={537}
+            height={480}
+            // style={{ width: "100%", height: "auto" }}
             priority
           />
         </div>

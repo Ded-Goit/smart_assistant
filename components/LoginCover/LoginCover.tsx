ff53a75614e6bb120c2a137629069c93c8ef0730
tina3008 <147944110+tina3008@users.noreply.github.com>
2025-09-03 22:19:13 +0300

    Update LoginCover.tsx
    add type

--- components/LoginCover/LoginCover.tsx
+++ components/LoginCover/LoginCover.tsx
@@ -1,14 +1,14 @@
-import Logout from "../Logout/Logout";
+import { ReactNode } from "react";
 import css from "./LoginCover.module.css";
 
-export default function LoginCover({ children }) {
+interface LoginCoverProps {
+  children: ReactNode;
+}
+
+export default function LoginCover({ children }: LoginCoverProps) {
   return (
     <div className={css.position}>
-      <div className={css.form}>
-        {children}
-        {/* <Logout /> */}
-          </div>
-
+      <div className={css.form}>{children}</div>
     </div>
   );
 }

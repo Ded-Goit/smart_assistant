8054565c460a149c772d231e01efb4c410af730d
tina3008 <kicja_3@ukr.net>
2025-09-04 20:41:43 +0300

    logout userinfo comp

---
+++ components/Logout/Logout.tsx
@@ -0,0 +1,21 @@
+import { logout } from "@/lib/api/apiClient";
+import css from "./LogOut.module.css";
+
+export default function Logout() {
+   const handleLogout = async () => {
+     try {
+       await logout();
+       window.location.href = "/";
+     } catch (err) {
+       console.error("Logout failed", err);
+     }
+   };
+
+  return (
+    <div className={css.button}>
+      <button type="button" className={css.btn} onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
+}

31c4836993202fc3e4dcd10901006bb3568b4363
jres <ljresetl@gmail.com>
2025-09-05 21:19:21 +0200

    rrrr

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -6,33 +6,47 @@ import Logo from "../Logo/logo";
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      {/* Логотип зверху */}
       <div className={styles.logo}>
-        <div><Logo width={158} height={56} /></div>
-      </div>
-
-      {/* Центральні кнопки */}
-      <nav className={styles.navCenter}>
-        <Link href="/" className={styles.navButton}>
-          Home
-        </Link>
-        <Link href="/calendar" className={styles.navButton}>
-          Calendar
-        </Link>
-        <Link href="/resourcesnew" className={styles.navButton}>
-          Resources
-        </Link>
-      </nav>
-
-      {/* Нижні кнопки */}
-      <div className={styles.navBottom}>
-        <Link href="/setting" className={styles.navButton}>
-          Setting
-        </Link>
-        <button className={styles.navButton} onClick={() => alert("Log out")}>
-          Log out
-        </button>
-      </div>
-    </aside>
+        <a href="" className={styles.mobile_menu_social_link}>
+          {/* <svg className={styles.social_icon_desk} width="210" height="72">
+              <use href="./images/icons.svg#icon-1"></use>
+            </svg> */}
+          </a>
+        </div>
+      <nav className={styles.menu}>
+        <a href="#" className={styles.active}>
+          {/* <svg className={styles.social_icon_menu} width="20" height="22">
+              <use href="./images/icons.svg#icon-Group"></use>
+            </svg> */}
+            Home
+          </a>
+        <a href="#" className={styles.active}>
+          {/* <svg className={styles.social_icon_menu} width="20" height="22">
+              <use href="./images/icons.svg#icon-Vector"></use>
+            </svg> */}
+            Calendar
+          </a>
+        <a href="#" className={styles.active}>
+          {/* <svg className={styles.social_icon_menu} width="20" height="22">
+              <use href="./images/icons.svg#icon-Vector-1"></use>
+            </svg> */}
+            Resources
+          </a>
+        </nav>
+      <div className={styles.menu_bottom}>
+        <a href="#" className={styles.menu_bottom_a}>
+          {/* <svg className={styles.social_icon_menu} width="20" height="22">
+              <use href="./images/icons.svg#icon-Vector-2"></use>
+            </svg> */}
+            Setting
+          </a>
+          <a href="#">
+          {/* <svg className={styles.social_icon_menu} width="20" height="22">
+              <use href="./images/icons.svg#icon-Group-1"></use>
+            </svg> */}
+            Log out
+          </a>
+        </div>
+      </aside>
   );
 }

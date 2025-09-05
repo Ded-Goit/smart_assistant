72e4191093c3c15deda1ac9a7466735ea2276199
Iryna Ielkina <ielkinairinka@gmail.com>
2025-09-05 16:30:41 +0300

    fix hero title

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -5,40 +5,38 @@ import Link from "next/link";
 export default function HeroPage() {
   return (
     <section className={styles.heroSection}>
-      <div className={styles.container}>
-        <div className={styles.hero}>
-          <div className={styles.hero__container}>
-            {/* Заголовок */}
-            <h1 className={`${styles.hero__title} h1`}>
-              Your smart productivity partner
-            </h1>
-            {/* Текст */}
-            <p className={`${styles.hero__text} text24`}>
-              Track your tasks, manage your time, and boost productivity with
-              powerful tools designed for students and professionals.
-            </p>
+      <div className={styles.hero}>
+        <div className={styles.hero__container}>
+          {/* Заголовок */}
+          <h1
+            className={`${styles.hero__title} h1`}
+            data-text="Your smart productivity partner"
+          >
+            Your smart productivity partner
+          </h1>
 
-            {/* Кнопка */}
-            <Link
-              href="/profile"
-              className={`${styles.hero__btn} btnfullcolor`}
-            >
-              Start Now
-            </Link>
-          </div>
+          {/* Текст */}
+          <p className={`${styles.hero__text} text24`}>
+            Track your tasks, manage your time, and boost productivity with
+            powerful tools designed for students and professionals.
+          </p>
 
-          {/* Зображення */}
-          <Image
-            className={styles.hero__image}
-            src="/hero/desk_girl_at_kanban.webp"
-            alt="Productivity illustration"
-            width={537}
-            height={739}
-            priority
-            style={{ width: "100%", height: "auto" }}
-            sizes="(max-width: 768px) 100vw, 537px"
-          />
+          {/* Кнопка */}
+          <Link href="/profile" className={`${styles.hero__btn} btnfullcolor`}>
+            Start Now
+          </Link>
         </div>
+        {/* Зображення */}
+        <Image
+          className={styles.hero__image}
+          src="/hero/desk_girl_at_kanban.webp"
+          alt="Productivity illustration"
+          width={537}
+          height={739}
+          priority
+          style={{ width: "100%", height: "auto" }}
+          sizes="(max-width: 768px) 100vw, 537px"
+        />
       </div>
     </section>
   );

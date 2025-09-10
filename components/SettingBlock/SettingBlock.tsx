1ed0c38968b6313f05f21ef1ac1e302963261df0
tina3008 <kicja_3@ukr.net>
2025-09-11 00:43:25 +0300

    setting blok

---
+++ components/SettingBlock/SettingBlock.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import ResetPasswordForm from "@/components/ResetPasswordForm/ResetPasswordForm";
+import styles from "./SettingBlock.module.css";
+import UserInfo from "@/components/UserInfo/UserInfo";
+import { Suspense } from "react";
+
+export default function SettingBlock() {
+    return (
+      <div className={styles.cover}>
+        <div className={styles.header}></div>
+        <div className={styles.mainBlock}>
+          <div className={styles.textlock}>
+            <h2 className={styles.titl}>Settings</h2>
+            <p className={styles.blabla}>
+              Update personal details, adjust notifications, and choose your
+              language settings
+            </p>
+          </div>
+          <div className={styles.bigBlock}>
+            <div className={styles.pwdBlock}>
+              <div className={styles.titleDecor}>
+                <h2 className={styles.title}>Password</h2>
+              </div>
+
+              <ResetPasswordForm />
+            </div>
+            <div className={styles.personal}>
+              <div className={styles.titleDecor}>
+                <h2 className={styles.title}>Personal info</h2>
+              </div>
+
+              <UserInfo />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}

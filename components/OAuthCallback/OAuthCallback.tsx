67a14a73dd364d356049c12e24dca00cd6f8ae8b
tina3008 <kicja_3@ukr.net>
2025-09-11 00:28:07 +0300

    Update OAuthCallback.tsx

--- components/OAuthCallback/OAuthCallback.tsx
+++ components/OAuthCallback/OAuthCallback.tsx
@@ -1,9 +1,46 @@
+// "use client";
+
+// import { useEffect } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { loginWithGoogle } from "@/lib/api/apiClient";
+// import css from "../GoogleLoginBtn/GoogleLoginBtn.module.css";
+
+// export default function OAuthCallback() {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+
+//   useEffect(() => {
+//     const code = searchParams.get("code");
+//     if (!code) return;
+
+//     const confirmOAuth = async () => {
+//       try {
+//         await loginWithGoogle(code);
+//         router.push("/setting");
+//       } catch (err) {
+//         console.error("err Google:", err);
+//       }
+//     };
+
+//     confirmOAuth();
+//   }, [searchParams, router]);
+
+//   return (
+//     <button className={css.googleBtn}>
+//       <svg className={css.googlIcon}>
+//         <use href="/sprite.svg#icon-google"></use>
+//       </svg>
+//       <p className={css.googlTxt}> Sign up with Google</p>
+//     </button>
+//   );
+// }
+
 "use client";
 
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { loginWithGoogle } from "@/lib/api/apiClient";
-import css from '../GoogleLoginBtn/GoogleLoginBtn.module.css'
+
 export default function OAuthCallback() {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -15,22 +52,15 @@ export default function OAuthCallback() {
     const confirmOAuth = async () => {
       try {
         await loginWithGoogle(code);
-        router.push("/"); 
+        router.push("/setting"); 
       } catch (err) {
-        console.error("err Google:", err);
+        console.error("Google OAuth error:", err);
+        router.push("/login"); 
       }
     };
 
     confirmOAuth();
   }, [searchParams, router]);
 
-  return (
-    <div className={css.googleBtn}>
-      <svg className={css.googlIcon}>
-        <use href="/sprite.svg#icon-google"></use>
-      </svg>
-      <p className={css.googlTxt}> Sign up with Google</p>
-    </div>
-  );
-  
+  return <p>Authorizing with Google...</p>;
 }

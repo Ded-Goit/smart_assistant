8e76710ff814a762e862c0880f28244c0d5dbd2c
tina3008 <kicja_3@ukr.net>
2025-09-10 23:47:14 +0300

    auth cover

--- components/AuthCover/AuthCover.tsx
+++ components/AuthCover/AuthCover.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, Suspense } from "react";
 import css from "./AuthCover.module.css";
 import Logo from "../Logo/logo";
+import Link from "next/link";
 
 interface AuthCoverProps {
   children: ReactNode;
@@ -10,16 +11,19 @@ export default function AuthCover({ children }: AuthCoverProps) {
   return (
     <div className={css.txtPart}>
       <div className={css.hederPart}>
-        <Logo/>
+        <Logo />
       </div>
       <Suspense fallback={<div>Loading...</div>}>
         <>{children}</>
       </Suspense>
       <div className={css.footerPart}>
-        <svg className={css.mailIcon}>
-          <use href="/sprite.svg#icon-mail"></use>
-        </svg>
-        <p className={css.footerTxt}>help@smartassistant.com</p>
+
+        <Link href="mailto:help@smartassistant.com" className={css.footerEmail}>
+          <svg className={css.mailIcon}>
+            <use href="/sprite.svg#icon-mail"></use>
+          </svg>
+          <p className={css.footerTxt}>help@smartassistant.com</p>
+        </Link>
       </div>
     </div>
   );

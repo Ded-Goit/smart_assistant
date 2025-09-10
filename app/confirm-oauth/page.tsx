3e18849b2c2a0e49a81ea8a1029d3155425cc438
tina3008 <kicja_3@ukr.net>
2025-09-11 01:01:58 +0300

    Update page.tsx

--- app/confirm-oauth/page.tsx
+++ app/confirm-oauth/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import css from "@/app/forgot-password/ForgotPasswordPage.module.css";
-// import OAuthCallback from "@/components/OAuthCallback/OAuthCallback";
+import OAuthCallback from "@/components/OAuthCallback/OAuthCallback";
 
 export default function GoogleAuthPage() {
   return (

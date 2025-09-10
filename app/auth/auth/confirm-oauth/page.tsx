bc1f5388607b9c31e12c7a93994cd5d631ffb78a
tina3008 <kicja_3@ukr.net>
2025-09-11 00:51:43 +0300

    auth

---
+++ app/auth/auth/confirm-oauth/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import css from "@/app/forgot-password/ForgotPasswordPage.module.css";
+import OAuthCallback from "@/components/OAuthCallback/OAuthCallback";
+
+export default function GoogleAuthPage() {
+  return (
+    <div className={css.cover}>
+      <OAuthCallback />
+    </div>
+  );
+}

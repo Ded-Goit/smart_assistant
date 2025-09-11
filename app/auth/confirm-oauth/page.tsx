01485c022529c79a7fbe7beee98813e5bb90e0f8
tina3008 <kicja_3@ukr.net>
2025-09-11 09:20:51 +0300

    Update page.tsx

--- app/auth/confirm-oauth/page.tsx
+++ app/auth/confirm-oauth/page.tsx
@@ -1,50 +1,50 @@
-// "use client";
-
-// import css from "@/app/forgot-password/ForgotPasswordPage.module.css";
-// import OAuthCallback from "@/components/OAuthCallback/OAuthCallback";
-
-// export default function GoogleAuthPage() {
-//   return (
-//      <div className={css.cover}>
-//       <OAuthCallback />
-//     </div>
-//   );
-// }
 "use client";
 
 import css from "@/app/forgot-password/ForgotPasswordPage.module.css";
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import OAuthCallback from "@/components/OAuthCallback/OAuthCallback";
 
-export default function OAuthCallback() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const code = searchParams.get("code");
-    if (!code) return;
-
-    const confirmOAuth = async () => {
-      try {
-        await fetch("/api/auth/google", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ code }),
-        });
-
-        router.push("/setting");
-      } catch (err) {
-        console.error("Google OAuth error:", err);
-        router.push("/login");
-      }
-    };
-
-    confirmOAuth();
-  }, [searchParams, router]);
-
+export default function GoogleAuthPage() {
   return (
-    <div className={css.cover}>
-      <p>Authorizing with Google...</p>
+     <div className={css.cover}>
+      {/* <OAuthCallback /> */}
     </div>
   );
 }
+// "use client";
+
+// import css from "@/app/forgot-password/ForgotPasswordPage.module.css";
+// import { useEffect } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// export default function OAuthCallback() {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+
+//   useEffect(() => {
+//     const code = searchParams.get("code");
+//     if (!code) return;
+
+//     const confirmOAuth = async () => {
+//       try {
+//         await fetch("/api/auth/google", {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify({ code }),
+//         });
+
+//         router.push("/setting");
+//       } catch (err) {
+//         console.error("Google OAuth error:", err);
+//         router.push("/login");
+//       }
+//     };
+
+//     confirmOAuth();
+//   }, [searchParams, router]);
+
+//   return (
+//     <div className={css.cover}>
+//       <p>Authorizing with Google...</p>
+//     </div>
+//   );
+// }
